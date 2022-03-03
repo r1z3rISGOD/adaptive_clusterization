ecc75210410fe6de0de0c7752626f816e68104c0
Kirill Kapytov <Kirill_Kapytov@epam.com>
2022-03-03 10:17:05 +0300

    DISSERT-2 Add xls parser

--- src/components/stupid/LoadFileBtn/LoadFileBtn.jsx
+++ src/components/stupid/LoadFileBtn/LoadFileBtn.jsx
@@ -1,8 +1,19 @@
 import React from "react"
+import {xlsParser} from "../../../utils/xlsParser";
 import './LoadFileBtn.css'
 
 export const LoadFileBtn = () => {
+    const handleFileUpload = (e) => {
+        e.preventDefault();
+        let file = e.target.files[0];
+        xlsParser(file).then(resp => console.log(resp));
+    }
     return(
-        <button className='load_file_btn'>Load new file</button>
+        <>
+            <button className='load_file_btn' onClick={() => document.getElementById('getFile').click()}>Upload new file</button>
+            <input id='getFile' className='load_file_input' type="file" accept=".xls,.xlsx"
+               onChange={event => handleFileUpload(event)}
+            />
+        </>
     )
 }
